9102b3ded483d946c07c2547fb9f25429ceeae06
mudimurtala <ibnmuhyideen95@gmail.com>
2024-07-11 12:27:39 +0100

    Add files

---
+++ server/routes/weatherRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getWeather } = require('../controllers/weatherController');
+const router = express.Router();
+
+router.get('/weather', getWeather);
+
+module.exports = router;
